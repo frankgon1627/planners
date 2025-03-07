c36d4a45dd98a58a732c37b2c483a26b5cee1939
frankgon1627 <fg1333762@gmail.com>
2025-03-07 01:06:33 -0500

    adding launch script

---
+++ launch/planners.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='planners',
+            executable='mpc_planner_corridors',
+            name='mpc_planner_corridors'
+        ),
+        Node(
+            package='planners',
+            executable='occupancy_grid_parser',
+            name='occupancy_grid_parser',
+            shell=True
+        ),
+        Node(
+            package='planners',
+            executable='jump_point_planner',
+            name='jump_point_planner',
+        )
+    ])
